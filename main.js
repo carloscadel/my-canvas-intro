140d0154d2981adb12efcc750dd92dc1f4cd84fd
carloscadel <c.cazallas.delfa@gmail.com>
2018-10-18 12:00:13 +0200

    draw a grid

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 console.log('hola')
-var canvas = document.getElementById('example');
+var canvas = document.getElementById('exampleCanvas');
 var ctx = canvas.getContext('2d');
 
 
@@ -7,7 +7,8 @@ function degreesToRadians(degrees) {
     return degrees*Math.PI/180;
 }
 
-function drawPacman(x, y, radius) {
+function drawPacman(x, y, radius, color='black') {
+    ctx.fillStyle = color;
     ctx.beginPath();
     ctx.arc(x, y, radius, degreesToRadians(320), degreesToRadians(40), true);
     ctx.lineTo(x, y);
@@ -15,17 +16,51 @@ function drawPacman(x, y, radius) {
     ctx.closePath()
 }
 
+function drawGrid(size = 20, color = 'rgba(255,0,0,0.5)'){
 
-// for(var y = 20; y <= 180; y += 20){
-//     drawPacman(50, y, 8);
-// }
+    ctx.strokeStyle = color
+    // var winHeight = window.innerHeight/80
+    var fontSize = (5+0.3*size)
 
-canvas.onclick = function(event){
-    console.log(event)
-    drawPacman(event.offsetX, event.offsetY, 10)
+    for(var y = 0; y < canvas.height; y += size){
+        ctx.beginPath()
+        ctx.moveTo(0, y)
+        ctx.lineTo(canvas.width, y)
+        ctx.stroke()
+        ctx.font = '' + (fontSize) + 'px' + ' monospace'
+        ctx.fillText(y, 0, y)
+
+    }
+    for(var x = 0; x < canvas.width; x += size){
+        ctx.beginPath()
+        ctx.moveTo(x, 0)
+        ctx.lineTo(x, canvas.height)
+        ctx.stroke()
+        ctx.textAlign = 'center'
+        ctx.font = '' + (fontSize) + 'px' + ' monospace'
+        ctx.fillText(x, x, fontSize)
+
+    }
+}
+drawGrid(100)
+
+
+ctx.font = '50px monospace'
+ctx.textAlign = 'center'
+ctx.fillText('Pacman', 500, 100)
+
+for(var y = 40; y <= 180; y += 20){
+    drawPacman(50, y, 8, 'rgb('+(y+20)+', 255, 0)');
 }
 
 
+
+// canvas.onclick = function(event){
+//     console.log(event)
+//     drawPacman(event.offsetX, event.offsetY, 10)
+// }
+
+// drawing pacman on click
 // ctx.beginPath()
 // ctx.arc(50, 100, 50, 0, degreesToRadians(360), false)
 // ctx.fill()
